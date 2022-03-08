aa7730497fbcc4d3c62c97177b582bf61f041099
tria124 <triulzi93@gmail.com>
2022-03-08 16:52:06 +0100

    added preprocesser

--- ClassifierV1.py
+++ ClassifierV1.py
@@ -2,9 +2,16 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+# for text preprocessing
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+
 
 class CategoryClassifier:
-    def __init__(self, trainin_data):
+    def __init__(self, training_data):
+        cleaned_training_data = __preprocess_sentences(training_data)
         category_dict = {
             "Location": 1,
             "Room": 2,
@@ -17,7 +24,7 @@ class CategoryClassifier:
         }
 
         category_list = []
-        for sent in trainin_data:
+        for sent in cleaned_training_data:
             sent_list = [sent[0], category_dict[sent[1]]]
             category_list.append(sent_list)
 
@@ -31,8 +38,53 @@ class CategoryClassifier:
         self.__nb_tfidf = MultinomialNB()
         self.__nb_tfidf.fit(X_train_vec_tfidf, y_train)
 
+    def __preprocess_sentences(self, data):
+        sent_list = []
+        for sentence in data:
+            sent_list.append(sentence[0])
+
+        for i in range(len(sent_list)):
+            sent_list[i] = sent_list[i].lower()
+            sent_list[i] = sent_list[i].replace(".", " ").replace(",", " ")
+
+        list_of_sentence = []
+        for sentence in sent_list:
+            no_stopwords = []
+            for word in sentence.split(" "):
+                if word not in stopwords.words("english"):
+                    no_stopwords.append(word)
+            list_of_sentence.append(" ".join(no_stopwords))
+
+        snow = SnowballStemmer("english")
+        list_of_stemmed_sentences = []
+        for sentence in list_of_sentence:
+            stemmed_sent = []
+            for word in word_tokenize(sentence):
+                s = snow.stem(word)
+                stemmed_sent.append(s)
+            list_of_stemmed_sentences.append(" ".join(stemmed_sent))
+
+        wordnet_lemmatizer = WordNetLemmatizer()
+        list_of_lemmatized_sentences = []
+        for sentence in list_of_lemmatized_sentences:
+            lemmatized_sent = []
+            for word in sentence.split(" "):
+                lemma = wordnet_lemmatizer.lemmatize(word)
+                lemmatized_sent.append(lemma)
+            list_of_lemmatized_sentences.append(" ".join(lemmatized_sent))
+
+        cleaned_data = []
+        for sentence in list_of_lemmatized_sentences:
+            for category in data:
+                final_list = [sentence, category[1]]
+                cleaned_data.append(final_list)
+
+        return cleaned_data
+
     def classify(self, test_data):
-        df_test = pd.DataFrame(test_data, columns=["sentence"])
+        cleaned_test_data = self.__preprocess_sentences(test_data)
+
+        df_test = pd.DataFrame(cleaned_test_data, columns=["sentence"])
         X_test = df_test["sentence"]
 
         X_test_vec_tfidf = self.__tfidf_vec.transform(X_test)
@@ -43,3 +95,14 @@ class CategoryClassifier:
         df_test["confidence"] = y_proba_test
 
         return df_test.values.tolist()
+
+
+if __name__ == "__main__":
+    from DataHandler.DataHandler import DataHandler
+    from sklearn.model_selection import train_test_split
+
+    my_data_handler = DataHandler()
+    category_list = my_data_handler.getCategorieData("Location")
+    for sent in category_list[1:10]:
+        print(f"{sent[0]:100}|{sent[1]}")
+
